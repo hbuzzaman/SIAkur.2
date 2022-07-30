b6969498bf378cea80504f7d963e5c0120619f79
hilmy uzz <hbuzzaman@gmail.com>
2022-07-30 12:30:07 +0700

    guarded id, kalibrasi

--- app/Kalibrasi.php
+++ app/Kalibrasi.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kalibrasi extends Model
 {
-    protected $fillable = ['alatukur_id', 'tgl_kalibrasi', 'tgl_nextkalibrasi', 'tgl_sertifikat', 'sertifikat', 'status'];
-
+    // protected $fillable = ['alatukur_id', 'tgl_kalibrasi', 'tgl_nextkalibrasi', 'tgl_sertifikat', 'sertifikat', 'status'];
+    protected $guarded =['id'];
     protected $hidden = ['created_at', 'updated_at'];
 
     public function alatukur()
